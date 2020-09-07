b31954037299d757bd2121da644a1dfb5ba51324
Pahud <pahudnet@gmail.com>
2020-09-07 21:45:44 +0800

    fix

--- bin/aga-multiples-regions-exmaple.ts
+++ bin/aga-multiples-regions-exmaple.ts
@@ -11,19 +11,19 @@ const envUS = { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-west-2' };
 
 // fargate in US
 const fargateUs = new SampleFargateStack(app, 'app-us', {env: envUS });
+// fargate in EU
+const fargateEu = new SampleFargateStack(app, 'app-eu', { env: envEU });
+
 
 // AGA in US
-new SampleAgaStack(app, 'app-us', {
+new SampleAgaStack(app, 'aga-us', {
   env: envUS,
   loadBalancer: fargateUs.loadBalancer,
 })
 
 
-// fargate in EU
-const fargateEu = new SampleFargateStack(app, 'app-eu', {env: envEU });
-
 // AGA in EU
-new SampleAgaStack(app, 'app-eu', {
+new SampleAgaStack(app, 'aga-eu', {
   env: envEU,
   loadBalancer: fargateEu.loadBalancer,
 })
